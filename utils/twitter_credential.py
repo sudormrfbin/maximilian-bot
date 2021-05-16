68f7ed382be7dc64cf6aeb7c739b836f1dd72ebf
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2021-05-17 01:23:37 +0530

    Update twitter_credential.py

--- utils/twitter_credential.py
+++ utils/twitter_credential.py
@@ -1,9 +1,9 @@
 #variables that contain the credentials to access the twitter api.
 
-consumer_key="Pemp33MJp1OTZLGPmHELMDVpB"
+consumer_key=""
 
-consumer_secret_key="D4r1L9JA5QV71IvICITy6nxcOacd0fz1kpvbEN7yBBVSZB3H1s"
+consumer_secret_key=""
 
-access_token="1393867050632716289-NcLRriTJguGqhYWlTM1hy91ZePTmQh"
+access_token=""
 
-access_secret_token="bnhvctZSfm7AwKMZWBkkRE25gupQiGnXCsk2S16K1nRil"
+access_secret_token=""
