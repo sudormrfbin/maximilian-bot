dd9425f191236a254b64e68b1718df69d7e4f9ba
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2021-05-17 19:15:22 +0530

    Updated certain search tags

--- utils/main1.py
+++ utils/main1.py
@@ -6,10 +6,10 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # Oauth keys
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "Pemp33MJp1OTZLGPmHELMDVpB"
+consumer_secret = "D4r1L9JA5QV71IvICITy6nxcOacd0fz1kpvbEN7yBBVSZB3H1s"
+access_token = "1393867050632716289-NcLRriTJguGqhYWlTM1hy91ZePTmQh"
+access_token_secret = "bnhvctZSfm7AwKMZWBkkRE25gupQiGnXCsk2S16K1nRil"
 
 # Authentication with Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -17,13 +17,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # update these for the tweet you want to process replies to 'name' = the account username and you can find the tweet id within the tweet URL
-name = 'Narendra Modi'
-user_id = '1075'
+name = 'narendramodi'
+tweet_id = '1393812701852303360'
 
 replies=[]
-for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(2):
-    if hasattr(tweet, 'in_reply_to_user_id_str'):
-        if (tweet.in_reply_to_user_id_str==user_id):
+for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(1000):
+    if hasattr(tweet, 'in_reply_to_status_id_str'):
+        if (tweet.in_reply_to_status_id_str==tweet_id):
             replies.append(tweet)
     print(tweet)
 
