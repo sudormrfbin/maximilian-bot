c43f800e57a46cb9ecc9023c5bc1e7ca8b99bec7
Gokul Soumya <gokulps15@gmail.com>
2021-05-20 08:48:46 +0530

    Send thread as direct message

--- bfh/__main__.py
+++ bfh/__main__.py
@@ -1,35 +1,5 @@
-import csv
-import tweepy
-import ssl
+# import csv
+# import tweepy
+# import ssl
 
-from tcreds import CONSUMER_KEY, CONSUMER_SECRET_KEY
-from tcreds import ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# Authentication with Twitter
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
-
-# update these for the tweet you want to process replies to
-# 'name' = the account username
-# you can find the tweet id within the tweet URL
-name = "narendramodi"
-tweet_id = "1393812701852303360"
-
-replies = []
-for tweet in tweepy.Cursor(
-    api.search, q=f"to:{name}", result_type="recent", timeout=999999
-).items(1000):
-    if hasattr(tweet, "in_reply_to_status_id_str"):
-        if tweet.in_reply_to_status_id_str == tweet_id:
-            replies.append(tweet)
-    print(tweet)
-
-with open("replies_clean.csv", "w") as f:
-    csv_writer = csv.DictWriter(f, fieldnames=("user", "text"))
-    csv_writer.writeheader()
-    for tweet in replies:
-        row = {"user": tweet.user.screen_name, "text": tweet.text.replace("\n", " ")}
-        csv_writer.writerow(row)
+# ssl._create_default_https_context = ssl._create_unverified_context
--- bfh/twit.py
+++ bfh/twit.py
@@ -2,10 +2,20 @@ from typing import List
 
 import tweepy
 
+from tcreds import CONSUMER_KEY, CONSUMER_SECRET_KEY
+from tcreds import ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from tcreds import BOTNAME
 
 # Terminology
 # status - tweet
 # thread - series of tweets from the same user posted as a continuous stream
+
+
+def construct_api() -> tweepy.API:
+    # Authentication with Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
 
 
 class BotMentionListener(tweepy.StreamListener):
@@ -17,10 +27,23 @@ class BotMentionListener(tweepy.StreamListener):
         """
         Executed when a new tweet is posted.
         """
-        if status.in_reply_to_status_id:
-            thread = Thread.from_tweet(
-                self.api, self.api.get_status(status.in_reply_to_status_id)
+        if not (status.in_reply_to_status_id and self.is_bot_mentioned(status)):
+            return
+
+        thread = Thread.from_tweet(
+            self.api, self.api.get_status(status.in_reply_to_status_id)
+        )
+        thread.send_as_direct_messages(status.user.id)
+
+    def is_bot_mentioned(self, status: tweepy.Status) -> bool:
+        """
+        Check if a tweet has mentioned our bot by @'ing.
+        """
+        return any(
+            filter(
+                lambda u: u["screen_name"] == BOTNAME, status.entities["user_mentions"]
             )
+        )
 
 
 class Thread:
@@ -28,13 +51,14 @@ class Thread:
     Represents a thread from a user. Stores only the last tweet in the thread.
     """
 
-    def __init__(self, api: tweepy.api.API, last_status: tweepy.Status):
+    def __init__(self, api: tweepy.API, last_status: tweepy.Status):
         self.api = api
         # In a thread every tweet by the authour is a reply to the previous
         # tweet in the thread. Storing the last tweet is easier since every
         # tweet has the id of the tweet it replies to -- so all we have to do
         # is work backwards.
         self.last_status = last_status
+        self.author = last_status.user.id
         self.thread: List[tweepy.Status] = []
 
         self.load_thread()
@@ -44,13 +68,16 @@ class Thread:
         if self.thread:
             return
 
-        status = self.last_status
+        # get full text of the tweet
+        status = self.api.get_status(self.last_status.id, tweet_mode="extended")
         self.thread.append(status)
 
         while True:
             # empty string if not reply
             if status.in_reply_to_status_id_str:
-                prev = self.api.get_status(status.in_reply_to_status_id)
+                prev = self.api.get_status(
+                    status.in_reply_to_status_id, tweet_mode="extended"
+                )
                 if prev.user.id == self.author:
                     status = prev
                     # we're traversing back up the thread from the end but need
@@ -62,7 +89,7 @@ class Thread:
                 break
 
     @classmethod
-    def from_tweet(cls, api: tweepy.api.API, status: tweepy.Status):
+    def from_tweet(cls, api: tweepy.API, status: tweepy.Status):
         """
         Construct a Thread object from any tweet that is part of the thread
         itself, i.e. find the beginning of the thread given any tweet in
@@ -78,9 +105,25 @@ class Thread:
 
         return cls(api, status)
 
+    def send_as_direct_messages(self, recipient_id):
+        for tweet in self.thread:
+            try:
+                text = tweet.full_text
+            except AttributeError:
+                text = tweet.text
+
+            self.api.send_direct_message(recipient_id, text)
+
     def __iter__(self):
         """
         Make this object iterable using a for loop, yielding individual
         statuses.
         """
         yield from self.thread
+
+
+if __name__ == "__main__":
+    api = construct_api()
+    streamlistener = BotMentionListener(api=api)
+    stream = tweepy.Stream(auth=api.auth, listener=streamlistener)
+    stream.filter(track=[BOTNAME])
